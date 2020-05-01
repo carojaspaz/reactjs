f2b9869e5bcd023c7cb7d8d6139cf8c2efae58e3
Carlos Rojas <carojaspaz@gmail.com>
2020-05-01 00:05:13 -0500

    202005010005

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//function Hello(props){
+//  return <h1>{props.title}</h1>
+//}
+
+//const Hello = (props) => <h2>{props.title}</h2>;
+
+class Hello extends Component {
+  render() {
+    return <h3>{this.props.title}</h3>;
+  }
+}
+
+class Text extends Component {
+  render() {
+    const { 
+      boolean, 
+      arrayOfNumber, 
+      arrayOfObject, 
+      text, 
+      number, 
+      multiply,
+      title
+    } = this.props;
+
+    const boolToText = boolean ? "Verdadero" : "Falso";
+    const mapNumber = arrayOfNumber.map(multiply);
+    return (
+      <div>
+        {title}
+        <p>{text}</p>
+        <p>{number}</p>
+        <p>{boolToText}</p>
+        <p>{mapNumber.join(', ')}</p>
+        <p>{arrayOfObject.key1}</p>
+      </div>
+    )
+  }
+}
+
+Text.defaultProps = {
+  text: "Texto por defecto"
+}
+
+class Counter extends Component{
+  constructor (props){
+    super(props)
+    this.state = { counter: this.props.initCount }
+    setInterval(()=>{
+      this.setState({counter: this.state.counter + 1});
+    },1000);
+  }
+  render(){
+    return <CounterNumber number={this.state.counter} />
+  }
+}
+
+Counter.defaultProps = {
+  initCount: 10
+}
+
+class CounterNumber extends Component{
+  render(){
+    return <span>Count: {this.props.number}</span>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter initCount={2}/>
+        <Text
+        text="Otro valor"
+        title={<h1>Titulo componente</h1>}
+        number="2" 
+        boolean 
+        arrayOfNumber={[2, 1, 30]} 
+        arrayOfObject={{key1:"Objeto 1", key2:"Objeto 2"}} 
+        multiply={(number) => number * 3}/>
       </header>
     </div>
   );
